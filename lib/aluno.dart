f13d3a86d0afbc066e8af76710246b1d6721c142
bialemess <cl201288@g.unicamp.br>
2023-09-05 10:46:12 -0300

    aa

---
+++ lib/aluno.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: unnecessary_this
+
+class Aluno {
+
+  int _ra = 0;
+  String _nome = "";
+
+  get ra => this._ra;
+
+ set ra( value) => this._ra = value;
+
+  get nome => this._nome;
+
+ set nome( value) => this._nome = value;
+
+Aluno (this._ra, this._nome);
+  
+
+
+
+
+}
--- lib/lista.dart
+++ lib/lista.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_4bia/aluno.dart';
 
 class MyLista extends StatefulWidget {
   const MyLista({super.key});
@@ -10,25 +11,33 @@ class MyLista extends StatefulWidget {
 }
 
 class _MyListaState extends State<MyLista> {
+  List<Aluno> lista = [];
+
   @override
   Widget build(BuildContext context) {
+    lista = [];
+    lista.add(Aluno(111, "Tania"));
+    lista.add(Aluno(222, "Priscila"));
+    lista.add(Aluno(333, "Matioli"));
+    lista.add(Aluno(444, "Simone"));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Exemplo Listview'),
       ),
       body: ListView.separated(
-        
-        separatorBuilder: (BuildContext context, int index) => Divider(thickness: 4, color: Colors.deepPurple,),
-        itemCount: 10, 
-        itemBuilder:(BuildContext context, int index) { 
+        separatorBuilder: (BuildContext context, int index) => Divider(
+          thickness: 4,
+        ),
+        itemCount: lista.length,
+        itemBuilder: (BuildContext context, int index) {
           return ListTile(
             leading: Icon(Icons.person),
-            title: Text("Testando"),
-            subtitle: Text("testando"),
-            trailing: Icon(Icons.person),
+            title: Text(lista[index].nome),
+            subtitle: Text(lista[index].ra.toString()),
+            trailing: Icon(Icons.book),
           );
-         }, 
-        
+        },
       ),
     );
   }
