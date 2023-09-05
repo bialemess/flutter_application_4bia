0e488421e40f5db14e06854e4eaf96023289f7b4
bialemess <cl201288@g.unicamp.br>
2023-09-05 10:19:07 -0300

    aa

---
+++ lib/lista.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MyLista extends StatefulWidget {
+  const MyLista({super.key});
+
+  @override
+  State<MyLista> createState() => _MyListaState();
+}
+
+class _MyListaState extends State<MyLista> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Exemplo Listview'),
+      ),
+      body: ListView.separated(
+        
+        separatorBuilder: (BuildContext context, int index) => Divider(thickness: 4, color: Colors.deepPurple,),
+        itemCount: 10, 
+        itemBuilder:(BuildContext context, int index) { 
+          return ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Testando"),
+            subtitle: Text("testando"),
+            trailing: Icon(Icons.arrow_forward_ios),
+          );
+         }, 
+        
+      ),
+    );
+  }
+}
